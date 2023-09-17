a2b79cd3d20cb06a7d5cfee56f34f48f013dfa5e
seyedex <dmitriydokerrr@gmail.com>
2023-09-17 17:01:57 +0300

    Added Tool Buttons Mockup

---
+++ imPhotoshop.WPF/Views/ToolPanelView.xaml.cs
@@ -0,0 +1,12 @@
+﻿
+using System.Windows.Controls;
+
+namespace imPhotoshop.WPF.Views;
+
+public partial class ToolPanelView : UserControl
+{
+    public ToolPanelView()
+    {
+        InitializeComponent();
+    }
+}
